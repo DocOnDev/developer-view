507e711c234bf6137c54aa71694dd49c15d12913
Doc Norton <doc@docondev.com>
2021-11-18 14:08:29 -0500

    Make list smaller/tighter
    Reduced list item margins and made list fit into view port

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <b-row>
-    <b-col style="padding: 1em;">
-      <b-list-group>
+    <b-col class="vh-100" style="padding: 1em;">
+      <b-list-group class="overflow-auto h-75">
         <span :key="c" v-for="(commit, c) in commits">
-          <b-list-group-item :to="'/'+commit.repoCommitId" v-bind:variant='scoreColor(commit.score)' class="d-flex justify-content-between align-items-center">
+          <b-list-group-item :to="'/'+commit.repoCommitId" v-bind:variant='scoreColor(commit.score)' class="d-flex justify-content-between align-items-center px-2 py-2">
             {{commit.commitMessage}}
             <b-badge v-bind:variant='scoreColor(commit.score)'>{{commit.score}}</b-badge>
           </b-list-group-item>
         </span>
       </b-list-group>
     </b-col>
-    <b-col cols="8" style="padding: 1em 2em 2em 1em;">
+    <b-col cols="8" class="py-2 pl-2 pr-4">
       <router-view/>
     </b-col>
   </b-row>
