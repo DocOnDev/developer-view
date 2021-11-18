43fc942e31a5431769ae060d9b222b04d6bbde14
Doc Norton <doc@docondev.com>
2021-11-18 14:32:10 -0500

    Narrow list
    Make list narrower and add ellipsis when text overflows

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <b-row>
-    <b-col class="vh-100" style="padding: 1em;">
+    <b-col cols="3" class="vh-100" style="padding: 1em;">
       <b-list-group class="overflow-auto h-75">
         <span :key="c" v-for="(commit, c) in commits">
           <b-list-group-item :to="'/'+commit.repoCommitId" v-bind:variant='scoreColor(commit.score)' class="d-flex justify-content-between align-items-center px-2 py-2">
-            {{commit.commitMessage}}
+            <span class="pr-2 d-inline-block text-truncate">{{commit.commitMessage}}</span>
             <b-badge v-bind:variant='scoreColor(commit.score)'>{{commit.score}}</b-badge>
           </b-list-group-item>
         </span>
       </b-list-group>
     </b-col>
-    <b-col cols="8" class="py-2 pl-2 pr-4">
+    <b-col class="py-3 pl-2 pr-3">
       <router-view/>
     </b-col>
   </b-row>
