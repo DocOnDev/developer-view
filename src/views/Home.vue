77e14f585d5b3cc77b9eb88f3434f803716b2679
Doc Norton <doc@docondev.com>
2021-11-21 16:22:20 -0500

    Home page has intro and links in commits for each repo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
   <b-row>
-    <b-col cols="3" class="vh-100" style="padding: 1em;">
-      <span :key="r" v-for="(repo, r) in repositories">
-        <b-card v-bind:header='repo.name' v-bind:border-variant='scoreColor(repo.commit[0].score)' header-text-variant="white" v-bind:header-bg-variant='scoreColor(repo.commit[0].score)'>
-          <b-link :to="{ name: 'repos', params: { slug: repo.slug } }" class="card-link">See Details</b-link>
-        </b-card>
-        <p />
-      </span>
+    <b-col>
+      <div>
+        <b-jumbotron header="Code Joy" lead="Code Joy is about the happiness of our code.">
+            <p>
+              First and foremost, our code is happy when we are happy with it.
+              So our first order of business is to keep track of how we feel about
+              the code we are working on.
+            </p>
+        </b-jumbotron>
+      </div>
+    </b-col>
+    <b-col style="padding: 1em;" cols="5">
+      <h4>Public Repositories</h4>
+      <p />
+      <b-card-group deck class="overflow-auto w-100">
+          <b-card class="mb-3" style="min-width: 200px; max-width: 200px;" :key="r" v-for="(repo, r) in repositories" v-bind:header='repo.name' v-bind:border-variant='scoreColor(repo.commit[0].score)' header-text-variant="white" v-bind:header-bg-variant='scoreColor(repo.commit[0].score)'>
+            <b-link :href='"/repos/"+repo.slug+"/"+repo.commit[0].repoCommitId'  class="card-link">{{repo.commit[0].subject}}</b-link>
+          </b-card>
+      </b-card-group>
     </b-col>
   </b-row>
 </template>
@@ -56,7 +68,11 @@
           commit(
             orderBy: createdAt_DESC
             first: 1
-          ) { score }
+          ) {
+            repoCommitId
+            score
+            subject
+          }
         }
       }
     `,
