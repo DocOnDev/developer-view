526ae4d929f339fc4417d763d891b269102ab477
Doc Norton <doc@docondev.com>
2021-12-20 18:09:26 -0500

    Move commit card into vue under commit component

---
+++ src/components/commit/CommitCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-col>
+    <div  :key="c" v-for="(commit, c) in commits">
+      <b-card :header="commit.repository.name + ' [' + commit.branch + '] - ' + commit.authors[0].name" v-bind:header-text-variant='textVariant(commit.score)' v-bind:header-bg-variant='scoreVariant(commit.score)' :sub-title="commit.subject" v-bind:border-variant='scoreVariant(commit.score)'>
+        <b-row>
+          <b-col>
+            {{commit.body}}
+          </b-col>
+          <b-col>
+            <div :key="f" v-for="(file, f) in commit.committedFiles">{{file}}</div>
+          </b-col>
+          <b-col cols="2">
+            <b-button :href="commit.repository.uri+'commit/'+commit.repoCommitId" target="_blank">See Original Commit</b-button>
+          </b-col>
+        </b-row>
+      </b-card>
+    </div>
+  </b-col>
+</template>
+
+<script>
+  import { GET_COMMIT } from '@/components/commit/queries';
+  import { SCORE_COLORS } from '@/mixins/score_colors';
+
+  export default {
+    name: "CommitCard",
+    props: ['commitId'],
+    mixins: [SCORE_COLORS],
+    apollo: {
+      commits: {
+        query: GET_COMMIT,
+        variables() {
+          return {
+            commitId: this.commitId
+          };
+        },
+      },
+    },
+
+  };
+
+</script>
--- src/views/Selected.vue
+++ src/views/Selected.vue
@@ -1,38 +1,18 @@
 <template>
   <b-row>
-    <b-col :key="c" v-for="(commit, c) in commits">
-      <b-card :header="commit.repository.name + ' [' + commit.branch + '] - ' + commit.authors[0].name" v-bind:header-text-variant='textVariant(commit.score)' v-bind:header-bg-variant='scoreVariant(commit.score)' :sub-title="commit.subject" v-bind:border-variant='scoreVariant(commit.score)'>
-        <b-row>
-          <b-col>
-            {{commit.body}}
-          </b-col>
-          <b-col>
-            <div :key="f" v-for="(file, f) in commit.committedFiles">{{file}}</div>
-          </b-col>
-          <b-col cols="2">
-            <b-button :href="commit.repository.uri+'commit/'+commit.repoCommitId" target="_blank">See Original Commit</b-button>
-          </b-col>
-        </b-row>
-      </b-card>
-    </b-col>
+    <CommitCard :commitId='commitId()' />
   </b-row>
 </template>
 
 <script>
-  import { GET_COMMIT } from '@/components/commit/queries';
-  import { SCORE_COLORS } from '@/mixins/score_colors';
+  import CommitCard from '@/components/commit/CommitCard.vue'
 
   export default {
-    mixins: [SCORE_COLORS],
-    apollo: {
-      commits: {
-        query: GET_COMMIT,
-        variables() {
-          return {
-            commitId: this.$route.params.commit
-          };
-        },
-      },
+    components: { CommitCard },
+    methods: {
+      commitId: function() {
+        return this.$route.params.commit
+      }
     },
   };
 </script>
