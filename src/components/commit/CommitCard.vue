243c7c3df44952489c142306edf5d30601dc1bf6
Doc Norton <doc@docondev.com>
2021-12-22 15:27:40 -0500

    Move commit body to subheader. Add branch button to header.

--- src/components/commit/CommitCard.vue
+++ src/components/commit/CommitCard.vue
@@ -1,10 +1,16 @@
 <template>
   <b-col>
     <div :key="c" v-for="(commit, c) in commits">
-      <b-card :header="commit.repository.name + ' [' + commit.branch + '] - ' + commit.authors[0].name" v-bind:header-text-variant='textVariant(commit.score)' v-bind:header-bg-variant='scoreVariant(commit.score)' :sub-title="commit.subject" v-bind:border-variant='scoreVariant(commit.score)'>
+      <b-card v-bind:header-text-variant='textVariant(commit.score)' v-bind:header-bg-variant='scoreVariant(commit.score)' :sub-title="commit.body" v-bind:border-variant='scoreVariant(commit.score)' header-tag="header" footer-tag="footer">
+        <template #header>
+          <span style="float: left; padding-top: .3em;">{{commit.subject}}</span>
+          <span style="float: right;">
+            <b-button href="#">{{commit.branch}}</b-button>
+          </span>
+        </template>
         <b-row>
           <b-col>
-            {{commit.body}}
+            {{commit.authors[0].name}}
           </b-col>
           <b-col>
             <div :key="f" v-for="(file, f) in commit.committedFiles">{{file}}</div>
@@ -13,10 +19,21 @@
             <b-button :href="commit.repository.uri+'commit/'+commit.repoCommitId" target="_blank">See Original Commit</b-button>
           </b-col>
         </b-row>
+        <template #footer>
+          Committed: {{formatDate(commit.createdAt)}}
+        </template>
       </b-card>
     </div>
   </b-col>
 </template>
+
+<style>
+.card-subtitle {
+    background-color: #ddd;
+    margin: -1.25em !important;
+    padding: 0.5em 1.25em;
+}
+</style>
 
 <script>
   import { GET_COMMIT } from '@/components/commit/queries';
@@ -26,6 +43,12 @@
     name: "CommitCard",
     props: ['commitId'],
     mixins: [SCORE_COLORS],
+    methods: {
+      formatDate: function(rawDate) {
+        let date = new Date(rawDate);
+        return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes()
+      }
+    },
     apollo: {
       commits: {
         query: GET_COMMIT,
